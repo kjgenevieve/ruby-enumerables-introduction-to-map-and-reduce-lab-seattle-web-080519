793e377ce1620ff928fc0333e4357e9eb86ce440
Genevieve McAllister <kj.genevieve@gmail.com>
2019-07-30 17:54:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -60,7 +60,17 @@ def reduce_to_all_true(source_array)
   return true
 end
 
-
+def reduce_to_any_true(source_array)
+  i = 0
+  while i < source_array.length do
+    # stop enumerating and return true if any element is true
+    if source_array[i] == true
+      return true
+    end
+    i += 1
+  end
+  return false
+end
 
 
 
