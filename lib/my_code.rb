8beb8141aaad64733d1005c9c443c58ef8e7a160
Genevieve McAllister <kj.genevieve@gmail.com>
2019-07-30 17:48:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -25,3 +25,26 @@ end
 def map_to_square(source_array)  
   
 end
+
+
+
+
+
+
+
+# def i_hear_a_sick_sound(passengers_sounds)
+#   i = 0 # set up a i for the enumeration of the passengers collection
+#   while i < passengers_sounds.length do # a loop for each passenger
+#     # Stop enumerating and return true if any passenger is
+#     # coughing or sneezing
+#     if (passengers_sounds[i] == "coughing" || passengers_sounds[i] == "sneezing")
+#       return true
+#     end
+#     i += 1
+#   end
+#   return false
+# end
+ 
+# i_hear_a_sick_sound(["coughing", "foo", "bar", "bin", "bat"]) #=> true
+# i_hear_a_sick_sound(["sneezing", "foo", "bar", "bin", "bat"]) #=> true
+# i_hear_a_sick_sound([            "foo", "bar", "bin", "bat"]) #=> false
